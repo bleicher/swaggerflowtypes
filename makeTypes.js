95fea996071d4d785d1c5fa5f0de0eb43b84f705
Markus Bleicher <bleicherm@sulzer.de>
2017-02-15 08:20:13 +0100

    Fixed a few bugs. Removed some very special "Betty Article" cases. Added a better "additionalProperty" handling to handle serialized Java Maps. Still not perfect.

--- makeTypes.js
+++ makeTypes.js
@@ -9,15 +9,32 @@ var fs = require('fs');
 var path = require('path');
 var request = require('request');
 
+     // var typeName = "TESTType";
+     // console.log('wrote to dir:' + options.output);
+     // var outPath = path.join('C:\\Users\\Bleicherm\\Desktop\\types', typeName + '.js');
+     // console.log('try writing to: ' + outPath);
+     // var outputBody = '/* @flow */\n' + "typeBody" + '\nmodule.exports = ' + typeName + ';\n';
+     // fs.writeFileSync(outPath, outputBody);
+     // console.log('wrote to: ' + outPath);
 
 
 var get = function(url, dataCallback) {
   request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      dataCallback(JSON.parse(body));
-    } else {
-      console.log(error);
-    }
+    if (error) {
+        console.log("Could not read url "+url);
+        console.log("************************************************** error");
+        console.log(error);
+        return;
+    };
+    if (response.statusCode != 200) {
+        console.log("Url "+url+" didn't return 200 but "+response.statusCode);
+        console.log("************************************************** response");
+        console.log(response);
+        console.log("************************************************** body");
+        console.log(body);
+        return;
+    };
+    dataCallback(JSON.parse(body));
   });
 };
 
@@ -48,13 +65,21 @@ var jsonSchemaTypeMap = {
       .map(function(value, key) {
         if (key === 'type') {
           // special handling for string-to-value mapping
-          return ['[key: string]', jsonSchemaTypeMap[value]];
-        } else if (key === '$ref' && isModelRef(value)) {
+          return ['[key: string]', jsonSchemaTypeMap[value.type||'object'](value, possibleImports)];
+        }
+		if (key === '$ref' && isModelRef(value)) {
           return ['[key: string]', extractModelNameFromRef(value)];
         } else {
           console.error( "don't know how to handle object with additionalProperties key '" + key + "'.");
           throw "don't know how to handle object with additionalProperties key '" + key + "'."
         }
+		if (key==='additionalProperties') {
+		  return ['[key: string]', jsonSchemaToFlowObject(object.additionalProperties, possibleImports)];
+		}
+        console.log( "don't know how to handle object with additionalProperties key '" + key + "' and value '"+value+"'.");
+		return ['[key: string]', {}]; //let's keep it simple ..., don't know if flow can do more than attaching {}
+//          console.error( "don't know how to handle object with additionalProperties key '" + key + "'.");
+//          throw "don't know how to handle object with additionalProperties key '" + key + "'."
       })
       .object();
   },
@@ -84,6 +109,8 @@ var jsonSchemaToFlowObject = function(schema, possibleImports) {
       result = modelName;
     } else {
       throw new Error('invalid schema:' + JSON.stringify(schema), 'no such type available: ' + schema.$ref);
+      console.log('invalid schema:' + JSON.stringify(schema), 'no such type available: ' + schema.$ref);
+      return 'any';
     }
   }
 
@@ -118,7 +145,7 @@ var outputAPI = function(modelSets) {
         .chain()
         .values()
         .map(function(model) {
-          //console.info( 'model: ', model );
+          console.info( 'model: ', model );
           // now we're dealing with the actual model
           var typeObject = _(model.properties)
             .chain()
@@ -127,10 +154,13 @@ var outputAPI = function(modelSets) {
             })
             .object()
             .value();
-          if (model.id == 'BettyArticle') {
-            console.log('now dealing with BettyArticle');
-            console.info( 'typeObject %d %s: ', ++counter, model.id, typeObject );
-          }
+//          if (model.id == 'BettyArticle') {
+//            console.log('now dealing with BettyArticle');
+//            console.info( 'typeObject %d %s: ', ++counter, model.id, typeObject );
+//          }
+		  console.log("***************************"+model.id);
+		  console.log(JSON.stringify(typeObject));
+		  console.log(typeObject);
           var classDefinition = JSON.stringify(typeObject)
             .replace(/\{/, '{\n  ')
             .replace(/,/g, ';\n  ')
@@ -152,12 +182,16 @@ var outputAPI = function(modelSets) {
             .value()
             .join('');
 
-          return imports + '\nclass ' + model.id + ' ' + classDefinition;
+          var result = imports + '\nclass ' + model.id + ' ' + classDefinition;
+		  console.log(result);
+		  return result;
         })
         .value();
     })
     .flatten()
     .each(function(typeBody) {
+      console.log('*************************************** writing');
+      console.log(typeBody);
       var typeName = typeBody.match(/class ([^ ]*) {/)[1];
       var outPath = path.join(options.output, typeName + '.js');
       var outputBody = '/* @flow */\n' + typeBody + '\nmodule.exports = ' + typeName + ';\n';
